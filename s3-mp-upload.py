d127ef2c26f358e4f4646abd8ce428044fd43d7b
Nathan Typanski <ntypanski@palantir.com>
2016-01-29 15:39:16 -0500

    format pep8

--- s3-mp-upload.py
+++ s3-mp-upload.py
@@ -11,22 +11,28 @@ import boto
 from boto.s3.connection import OrdinaryCallingFormat
 
 parser = argparse.ArgumentParser(description="Transfer large files to S3",
-        prog="s3-mp-upload")
+                                 prog="s3-mp-upload")
 parser.add_argument("src", type=file, help="The file to transfer")
 parser.add_argument("dest", help="The S3 destination object")
 parser.add_argument("-np", "--num-processes", help="Number of processors to use",
-        type=int, default=2)
+                    type=int, default=2)
 parser.add_argument("-f", "--force", help="Overwrite an existing S3 key",
-        action="store_true")
-parser.add_argument("-s", "--split", help="Split size, in Mb", type=int, default=50)
-parser.add_argument("-rrs", "--reduced-redundancy", help="Use reduced redundancy storage. Default is standard.", default=False,  action="store_true")
+                    action="store_true")
+parser.add_argument(
+    "-s", "--split", help="Split size, in Mb", type=int, default=50)
+parser.add_argument("-rrs", "--reduced-redundancy",
+                    help="Use reduced redundancy storage. Default is standard.", default=False,  action="store_true")
 parser.add_argument("--insecure", dest='secure', help="Use HTTP for connection",
-        default=True, action="store_false")
-parser.add_argument("-t", "--max-tries", help="Max allowed retries for http timeout", type=int, default=5)
-parser.add_argument("-v", "--verbose", help="Be more verbose", default=False, action="store_true")
-parser.add_argument("-q", "--quiet", help="Be less verbose (for use in cron jobs)", default=False, action="store_true")
+                    default=True, action="store_false")
+parser.add_argument("-t", "--max-tries",
+                    help="Max allowed retries for http timeout", type=int, default=5)
+parser.add_argument("-v", "--verbose", help="Be more verbose",
+                    default=False, action="store_true")
+parser.add_argument("-q", "--quiet", help="Be less verbose (for use in cron jobs)",
+                    default=False, action="store_true")
 
 logger = logging.getLogger("s3-mp-upload")
+
 
 def do_part_upload(args):
     """
@@ -68,26 +74,30 @@ def do_part_upload(args):
     if not data:
         raise Exception("Unexpectedly tried to read an empty chunk")
 
-    def progress(x,y):
-        logger.debug("Part %d: %0.2f%%" % (i+1, 100.*x/y))
+    def progress(x, y):
+        logger.debug("Part %d: %0.2f%%" % (i + 1, 100. * x / y))
 
     try:
         # Do the upload
         t1 = time.time()
-        mpu.upload_part_from_file(StringIO(data), i+1, cb=progress)
+        mpu.upload_part_from_file(StringIO(data), i + 1, cb=progress)
 
         # Print some timings
         t2 = time.time() - t1
-        s = len(data)/1024./1024.
-        logger.info("Uploaded part %s (%0.2fM) in %0.2fs at %0.2fMBps" % (i+1, s, t2, s/t2))
+        s = len(data) / 1024. / 1024.
+        logger.info("Uploaded part %s (%0.2fM) in %0.2fs at %0.2fMBps" %
+                    (i + 1, s, t2, s / t2))
     except Exception, err:
-        logger.debug("Retry request %d of max %d times" % (current_tries, max_tries))
+        logger.debug("Retry request %d of max %d times" %
+                     (current_tries, max_tries))
         if (current_tries > max_tries):
             logger.error(err)
         else:
             time.sleep(3)
             current_tries += 1
-            do_part_download(bucket_name, mpu_id, fname, i, start, size, secure, max_tries, current_tries)
+            do_part_download(bucket_name, mpu_id, fname, i,
+                             start, size, secure, max_tries, current_tries)
+
 
 def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=False, verbose=False, quiet=False, secure=True, max_tries=5):
     # Check that dest is a valid S3 url
@@ -104,56 +114,60 @@ def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=F
     # See if we're overwriting an existing key
     if key is not None:
         if not force:
-            raise ValueError("'%s' already exists. Specify -f to overwrite it" % dest)
+            raise ValueError(
+                "'%s' already exists. Specify -f to overwrite it" % dest)
 
     # Determine the splits
-    part_size = max(5*1024*1024, 1024*1024*split)
-    src.seek(0,2)
+    part_size = max(5 * 1024 * 1024, 1024 * 1024 * split)
+    src.seek(0, 2)
     size = src.tell()
     num_parts = int(ceil(size / part_size))
 
     # If file is less than 5M, just upload it directly
-    if size < 5*1024*1024:
+    if size < 5 * 1024 * 1024:
         src.seek(0)
         t1 = time.time()
-        k = boto.s3.key.Key(bucket,split_rs.path)
+        k = boto.s3.key.Key(bucket, split_rs.path)
         k.set_contents_from_file(src)
         t2 = time.time() - t1
-        s = size/1024./1024.
-        logger.info("Finished uploading %0.2fM in %0.2fs (%0.2fMBps)" % (s, t2, s/t2))
+        s = size / 1024. / 1024.
+        logger.info("Finished uploading %0.2fM in %0.2fs (%0.2fMBps)" %
+                    (s, t2, s / t2))
         return
 
     # Create the multi-part upload object
-    mpu = bucket.initiate_multipart_upload(split_rs.path, reduced_redundancy=reduced_redundancy)
+    mpu = bucket.initiate_multipart_upload(
+        split_rs.path, reduced_redundancy=reduced_redundancy)
     logger.info("Initialized upload: %s" % mpu.id)
 
     # Generate arguments for invocations of do_part_upload
     def gen_args(num_parts, fold_last):
-        for i in range(num_parts+1):
-            part_start = part_size*i
-            if i == (num_parts-1) and fold_last is True:
-                yield (bucket.name, mpu.id, src.name, i, part_start, part_size*2, secure, max_tries, 0)
+        for i in range(num_parts + 1):
+            part_start = part_size * i
+            if i == (num_parts - 1) and fold_last is True:
+                yield (bucket.name, mpu.id, src.name, i, part_start, part_size * 2, secure, max_tries, 0)
                 break
             else:
                 yield (bucket.name, mpu.id, src.name, i, part_start, part_size, secure, max_tries, 0)
 
-
     # If the last part is less than 5M, just fold it into the previous part
-    fold_last = ((size % part_size) < 5*1024*1024)
+    fold_last = ((size % part_size) < 5 * 1024 * 1024)
 
     # Do the thing
     try:
         # Create a pool of workers
         pool = Pool(processes=num_processes)
         t1 = time.time()
-        pool.map_async(do_part_upload, gen_args(num_parts, fold_last)).get(9999999)
+        pool.map_async(do_part_upload, gen_args(
+            num_parts, fold_last)).get(9999999)
         # Print out some timings
         t2 = time.time() - t1
-        s = size/1024./1024.
+        s = size / 1024. / 1024.
         # Finalize
         src.close()
         mpu.complete_upload()
-        logger.info("Finished uploading %0.2fM in %0.2fs (%0.2fMBps)" % (s, t2, s/t2))
+        logger.info("Finished uploading %0.2fM in %0.2fs (%0.2fMBps)" %
+                    (s, t2, s / t2))
     except KeyboardInterrupt:
         logger.warn("Received KeyboardInterrupt, canceling upload")
         pool.terminate()
